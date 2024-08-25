831a585bad8c5b0b154b837dc20cadea19393a20
123456789degrees <126020408+123456789degrees@users.noreply.github.com>
2024-08-25 04:25:20 +0000

    Testing: Just finished 0c3 turtle tricks. There are some typos I found in the file 04_Check_In_Your_Code.ipynb, where it says accecss instead of access.

--- lessons/00_Turtles/03c_Turtle_Tricks.py
+++ lessons/00_Turtles/03c_Turtle_Tricks.py
@@ -16,8 +16,14 @@ turtle.setup (width=600, height=600)    # Set the size of the window
 tina = turtle.Turtle()                  # Create a turtle named tina
 
 # Use tina.circle() to draw a circle, and tina.goto() to move tina to a new location
-# Use tina.begin_fill(), tina.end_fill(), and tina.fillcolor() to fill in the shapes
 
+# Use tina.begin_fill(), tina.end_fill(), and tina.fillcolor() to fill in the shapes
+tina.fillcolor("Blue")
+tina.begin_fill()
+tina.circle(100)
+tina.end_fill()
+tina.penup()
+tina.goto(700, 700)
 
 ... # Your code here
 

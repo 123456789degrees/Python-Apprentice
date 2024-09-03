58e0d1b62b96812c3fe6e14d225f9cc0e247a26a
123456789degrees <126020408+123456789degrees@users.noreply.github.com>
2024-09-03 22:40:28 +0000

    Finished 05c_loop with turtle

--- lessons/00_Turtles/05c_Loop_with_Turtle.py
+++ lessons/00_Turtles/05c_Loop_with_Turtle.py
@@ -1,11 +1,8 @@
+import turtle 
+turtle.setup (width=600, height=600)
 
-"""
-Turtles with a loop. 
-
-Study the previous program, 05a_Loop_with_Turtle.py, and then
-write a new program that uses a loop to draw a pentagon.
-( You can cut and past most of it! )
-
-"""
-
-... # Your code here
+tina = turtle.Turtle()
+for i in range (5):
+    tina.forward(100)
+    tina.left(72)
+turtle.exitonclick()   

39c5265841395e2a3274d38ca99b688296b0e1bf
123456789degrees <126020408+123456789degrees@users.noreply.github.com>
2024-09-11 04:21:01 +0000

    Did all lessons up to lessons/00_Turtles/09_LeagueBot.py. Also added an issue.

--- lessons/00_Turtles/09_LeagueBot.py
+++ lessons/00_Turtles/09_LeagueBot.py
@@ -10,11 +10,27 @@ Write your own turtle program! Here is what your program should do
 """
 
 import turtle as turtle
+def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
 
 screen = turtle.Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('white')
 
 t = turtle.Turtle()
-
+set_turtle_image(t, "leaguebot_bolt.gif")
+t.shapesize
+t.color("Blue")
+for i in range (6):
+    t.forward(100)
+    t.left(60)
 ... # Your Code Here
+turtle.exitonclick()   
